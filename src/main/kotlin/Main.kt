e368b192eccae9d4beba529be9ddf2a7f6500328
0ut0f4mm0 <eriking2004@gmail.com>
2023-01-12 12:16:50 +0100

    Casi acabado 6.1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,1 +1,34 @@
-fun main() {}
+fun main() {
+    val gun= Gun(".45 magnum")
+    gun.shoot()
+    val shotgun = Shotgun("Double Barrel")
+    shotgun.shoot()
+    val rifle = Rifle("AK-47")
+    rifle.shoot()
+    rifle.shootMode()
+}
+
+open class Gun(){
+    var gunModel = ""
+    constructor(gunModel:String) : this(){
+        this.gunModel = gunModel
+    }
+    open fun shoot(){
+        println("The $gunModel gun shot losing all its bullets")
+    }
+}
+
+class Shotgun(gunModel: String): Gun(gunModel){
+    override fun shoot() {
+        println("The $gunModel shotgun shot losing all its .12 bullets")
+    }
+}
+
+class Rifle(gunModel: String): Gun(gunModel){
+    override fun shoot() {
+        println("The $gunModel rifle shot losing all its 7.62 bullets")
+    }
+    fun shootMode(){
+        println("The $gunModel rifle can shoot in automatic and semiautomatic mode.")
+    }
+}
